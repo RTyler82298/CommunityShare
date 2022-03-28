4d109a7a6b846ad6e36c39626b461031be050b18
Duskid123 <75815814+Duskid123@users.noreply.github.com>
2022-03-28 18:16:02 -0400

    Update Beta ride db.sql
    added passengers allowed to the list

--- src/Beta ride db.sql
+++ src/Beta ride db.sql
@@ -5,9 +5,14 @@ GoingTo Varchar(50),
 UserName Varchar(30),
 Community Varchar(30),
 Gender varchar(20) not null Check(Gender In('Male', 'Female', 'Mixed'))
+ --, passangerAvalable int
 );
 ALTER TABLE Rides  
 WITH NOCHECK ADD  CONSTRAINT FK_Rides_Users
 FOREIGN KEY(UserName)
 REFERENCES Users (UserName)
 ON UPDATE CASCADE;
+
+ALTER TABLE rides
+ADD passangerAvalable int;
+-- or you can add it to the inital making of the table
